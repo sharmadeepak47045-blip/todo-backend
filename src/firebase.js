915a40178cdb78ecbd1b58c3372e981b9fb1fb17
Developer 1 <sharmadeepak47045@gmail.com>
2025-12-19 21:14:13 +0530

    backend deploy fixes

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,3 @@
-// src/firebase.js
 import admin from "firebase-admin";
 import fs from "fs";
 import path from "path";
@@ -6,12 +5,11 @@ import path from "path";
 // File ka path
 const serviceAccountPath = path.join(process.cwd(), 'src/serviceAccountKey.json');
 
-// File exist check
+
 if (!fs.existsSync(serviceAccountPath)) {
   throw new Error("serviceAccountKey.json file not found! Make sure it is in the src folder.");
 }
 
-// Read JSON
 const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf-8'));
 
 // Initialize Firebase Admin
