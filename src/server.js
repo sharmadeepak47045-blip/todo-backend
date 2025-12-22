3555456b9c6ec4c62527fbd060ac25b8371202b2
Developer 1 <sharmadeepak47045@gmail.com>
2025-12-22 16:52:02 +0530

    backend deploy fixes

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173","http://localhost:5174","http://localhost:5175",
+    origin: ["http://localhost:3000", "http://localhost:5173","http://localhost:5174","http://localhost:5175","https://todo-frontend-delta-tawny.vercel.app",
       process.env.FRONTEND_URL],
     credentials: true,
   })
